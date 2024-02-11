a35db36044b5a01ba9a8553fc2fd79daf7b4999e
viictor-m <magalhaes.a.victor@gmail.com>
2024-02-11 19:12:52 +0000

    :construction: rotas para requisições de clientes

---
+++ src/clientes/rotas.py
@@ -0,0 +1,11 @@
+"""Módulo de rotas da API."""
+
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from src.clientes.esquemas import CorpoTransacao
+
+rotas = APIRouter()
+
+rotas.post("/clientes/{id}/transacoes") 
+def transacionar(id: int, corpo: CorpoTransacao) -> JSONResponse:
+    return JSONResponse(content=f"Lógica não implementada. id: {id} | corpo: {corpo}")
