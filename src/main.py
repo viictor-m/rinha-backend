51b7ad47972672c205792cd3b1bb7298b6559d9e
viictor-m <magalhaes.a.victor@gmail.com>
2024-02-11 20:52:37 +0000

    :hammer: adiciona rotas de clientes e handler de erros

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
 """Módulo responsável por rodar a API."""
 
+from importlib import import_module
+
 from fastapi import FastAPI
+
 from src.clientes.rotas import rotas
 
 
 app = FastAPI(title="API de crébitos da rinha de backend.")
+
+app.include_router(rotas, prefix="/clientes", tags=["clientes"])
+
+import_module("src.erros")
 
 
 @app.get("/")
@@ -18,5 +25,3 @@ def hello() -> str:
         Mensagem de boas-vindas.
     """
     return "API de crébitos da rinha de backend está on e roteando!"
-
-app.include_router(rotas)
