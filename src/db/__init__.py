074792a9b8e9f7ea87ff38848ef6304e0cf56fa9
viictor-m <magalhaes.a.victor@gmail.com>
2024-02-11 20:54:02 +0000

    :rocket: conexão com banco de dados

---
+++ src/db/__init__.py
@@ -0,0 +1,19 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+from src import config
+from src.logit import log
+
+
+cliente = MongoClient(
+    config.db.mongodb_uri, server_api=ServerApi(config.db.mongodb_api_servidor)
+)
+banco = cliente[config.db.banco]
+
+try:
+    cliente.admin.command("ping")
+except Exception as e:
+    log.critical("Erro ao estabelecer conexão com o banco de dados.")
+    log.exception(e)
+else:
+    log.info("Conexão com o banco de dados estabelecida!")
