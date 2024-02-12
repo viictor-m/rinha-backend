fccc6e235d5c5bf825685059a763e372b2571ef9
viictor-m <vmagalhaes@id.uff.br>
2024-02-12 17:31:08 -0300

    :rocket: lógica da transação implementada

--- src/clientes/rotas.py
+++ src/clientes/rotas.py
@@ -1,8 +1,10 @@
 """Módulo de rotas da API."""
 
 from fastapi import APIRouter
+from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
+from src.clientes.acoes import fazer_transacao
 from src.clientes.esquemas import CorpoTransacao
 
 
@@ -11,4 +13,23 @@ rotas = APIRouter()
 
 @rotas.post("/{id}/transacoes")
 def transacionar(id: int, corpo: CorpoTransacao) -> JSONResponse:
-    return JSONResponse(content=f"Lógica não implementada. id: {id} | corpo: {corpo}")
+    """
+    Realiza a transação pedida ao ID de entrada.
+
+    Transação só realmente será realizada se houver saldo suficiente disponível para
+    tal. Caso contrário, esta não será completa.
+
+    Parameters
+    ----------
+    id : int
+        ID do usuário para transacionar.
+    corpo : CorpoTransacao
+        Informações da transação.
+
+    Returns
+    -------
+    JSONResponse
+        Reposta contendo limite e saldo atualizado do usuário.
+    """
+    novos_valores = fazer_transacao(id, corpo)
+    return JSONResponse(content=jsonable_encoder(novos_valores))
