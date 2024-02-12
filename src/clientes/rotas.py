b84565089dc878de5daadb3e9ce8406daa0d5836
viictor-m <vmagalhaes@id.uff.br>
2024-02-12 18:22:12 -0300

    :rocket: novo endpoint de extrato

--- src/clientes/rotas.py
+++ src/clientes/rotas.py
@@ -5,6 +5,7 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 
 from src.clientes.acoes import fazer_transacao
+from src.clientes.acoes import gerar_extrato
 from src.clientes.esquemas import CorpoTransacao
 
 
@@ -33,3 +34,22 @@ def transacionar(id: int, corpo: CorpoTransacao) -> JSONResponse:
     """
     novos_valores = fazer_transacao(id, corpo)
     return JSONResponse(content=jsonable_encoder(novos_valores))
+
+
+@rotas.get("/{id}/extrato")
+def extrato(id: int) -> JSONResponse:
+    """
+    Gera extrato do cliente.
+
+    Parameters
+    ----------
+    id : int
+        ID do cliente.
+
+    Returns
+    -------
+    JSONResponse
+        Resposta com valores de limite, saldo e últimas 10 transações.
+    """
+    valores = gerar_extrato(id)
+    return JSONResponse(content=jsonable_encoder(valores))
